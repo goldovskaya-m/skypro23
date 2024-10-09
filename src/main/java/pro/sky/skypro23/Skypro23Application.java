60c5712544066fb3382073126efe9ba16d2e3f7a
goldovskaya-m <ariya-aaa@yandex.ru>
2024-10-10 00:12:01 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skypro23/Skypro23Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skypro23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skypro23Application.class, args);
+	}
+
+}
