b0cb0da650b6cac36d6f7744d27dc3cc38323287
goldovskaya-m <ariya-aaa@yandex.ru>
2024-10-10 20:18:24 +0300

    Homework Done + pom

---
+++ src/test/java/pro/sky/skypro23/Skypro23ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skypro23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
